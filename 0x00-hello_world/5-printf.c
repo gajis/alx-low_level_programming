4176cd7b2880d279b5385f5c38a573cba6bf7d44
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-02-17 14:57:28 -0800

    positive or negative

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int (void)
+/**
+ *main - Entry point
+ * 
+ *return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
