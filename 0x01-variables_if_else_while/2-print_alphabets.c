e4f1222e5b5048e5d127ccf27ab62feab743d51d
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-02-18 16:04:45 -0800

    print lower case alpabets

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description - prog display a-z
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+		putchar ('\n');
+	}
+
+	return (0);
+}
+
