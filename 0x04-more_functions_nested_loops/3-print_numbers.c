9a68ee8455c245e104a949e3e8c49dee59364746
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-02-23 14:11:55 -0800

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ *print_numbers - print 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char x = 0;
+
+	while (x <= 9)
+	{
+		_putchar(x + '0');
+		x++;
+	}
+	_putchar('\n');
+
+}
