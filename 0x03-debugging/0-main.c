2f817c76467148eb247031dc26b2c7a5db963e41
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-02-24 09:23:28 -0800

    1 main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function if integer is positive or not
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+
+x = 0;
+positive_or_negative(x);
+
+return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+0 Function that test positive or negative with correct output
+1 comment out part of a code
