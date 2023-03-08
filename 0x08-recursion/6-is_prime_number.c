9613f1dd44ca068c21a74360e4ebc22716685cf2
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-03-08 14:02:41 -0800

    6 is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_number -  Function that returns 1 if integer is prime
+ * @n: integer
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	int x = 2;
+
+	if (n == 0 || n == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		if (n % 2 != 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (is_prime_number(x++));
+		}
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 This is Recursion project
 0 function that returns the factorial of a given number
+6 function that returns 1 if the input integer is a prime number
