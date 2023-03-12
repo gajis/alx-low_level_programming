138b243c935d15c4912cb37164bf2572257c394a
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-03-12 06:56:10 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _counter - Function that prints number of arguments passed into it
+ * @argc: counter
+ * @argv: pointer
+ * Return: 0
+ */
+int _counter(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+1 Program that prints number of arguments passed into it
