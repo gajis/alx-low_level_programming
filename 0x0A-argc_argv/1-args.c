61db115eb2fdb3b30b6e87673f22a2593dabf45b
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-03-15 07:16:26 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-
 /**
- * print_num - Function that prints number of arguments passed into it
+ * main - Function that prints number of arguments passed into it
  * @argc: counter
  * @argv: pointer
  * Return: 0
  */
-int print_num(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	_putchar(argc);
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
