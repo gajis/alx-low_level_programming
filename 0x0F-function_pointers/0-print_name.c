35022edff6a38b9754881e1819040800ae4d2391
Ahmed Rufai Abubakar <specialoneonly@gmail.com>
2023-03-22 07:37:43 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name
- * @name: pointer
- * @f: pointer
+ * array_iterator - function that prints a name
+ * @array: array
+ * @size: size
+ * @action: pointer
  */
-
-void print_name(char *name, void(*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
 
-	if (name != NULL && f != NULL)
-		f(name);
+	if (!array || !action)
+		return;
 
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,2 @@
 0 function that prints a name
+1 function that executes a function given as parameter
